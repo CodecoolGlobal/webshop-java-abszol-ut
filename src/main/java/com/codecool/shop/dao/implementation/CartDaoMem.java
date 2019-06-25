d2d80a0714e215b815e6ce25522364cd3aa3bb8d
Gergely Simonics <gergelysimonics@Gergelys-MacBook-Pro.local>
2019-06-25 13:24:12 +0200

    added shopping cart

---
+++ src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
@@ -0,0 +1,46 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.model.Cart;
+import com.codecool.shop.model.Product;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartDaoMem implements CartDao {
+
+    private List<Product> data = new ArrayList<>();
+    private static CartDaoMem instance = null;
+
+    private CartDaoMem() {
+    }
+
+    public static CartDaoMem getInstance() {
+        if (instance == null) {
+            instance = new CartDaoMem();
+        }
+        return instance;
+    }
+
+    @Override
+    public void add(Product product) {
+        product.setId(data.size() + 1);
+        data.add(product);
+    }
+
+    @Override
+    public Product find(int id) {
+        return data.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+    }
+
+    @Override
+    public void remove(int id) {
+        data.remove(find(id));
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return data;
+    }
+}
