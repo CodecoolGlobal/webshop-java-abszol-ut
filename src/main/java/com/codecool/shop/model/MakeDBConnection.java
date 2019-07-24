2d52ba2e2a7991daa2d4790fab8b08ccb40abd2f
Gergely Simonics <gergelysimonics@Gergelys-MacBook-Pro.local>
2019-07-24 10:59:56 +0200

    created connection.properties file and changed the MakeDBConnection file, so it will work without environment variables now

--- src/main/java/com/codecool/shop/model/MakeDBConnection.java
+++ src/main/java/com/codecool/shop/model/MakeDBConnection.java
@@ -1,21 +1,33 @@
 package com.codecool.shop.model;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class MakeDBConnection {
 
-    private static final String DATABASE = System.getProperty("db.url");
-    private static final String DB_USER = System.getProperty("db.user");
-    private static final String DB_PASSWORD = System.getProperty("db.pass");
-
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(
-                DATABASE,
-                DB_USER,
-                DB_PASSWORD);
+        try {
+
+            Properties props = new Properties();
+            String dbSettingsPropertyFile = "src/main/resources/connection.properties";
+            FileReader fReader = new FileReader(dbSettingsPropertyFile);
+
+            props.load(fReader);
+
+            String dbDriverClass = props.getProperty("db.driver.class");
+            String dbConnUrl = props.getProperty("db.conn.url");
+            String dbUserName = props.getProperty("db.username");
+            String dbPassword = props.getProperty("db.password");
+
+            Class.forName(dbDriverClass);
+
+            return DriverManager.getConnection(dbConnUrl, dbUserName, dbPassword);
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException("Could not read db connection file", e);
+        }
     }
-
-
 }
